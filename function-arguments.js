686b5a7bb805ed2f78e67a9b22dfb027405765bb
Jimmy Chicaiza <jimmy.chicaiza@gmail.com>
2019-03-14 19:22:55 -0400

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,9 @@
+function math (a, b, c) { //arguments are only as reference
+
+return b * c + a;
+
+}
+
+console.log(math(53, 61, 67));
+
+
---
+++ strings.js
@@ -0,0 +1,3 @@
+var myString = 'this is a string';
+
+console.log(myString);
